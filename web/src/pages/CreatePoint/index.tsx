f9a664020f46c33624c6d0573bc6cf322bf2e3c4
dpisati <dpisati@gmail.com>
2020-08-20 13:04:31 +1200

    address_location

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,4 +1,10 @@
-import React, { useEffect, useState, ChangeEvent, FormEvent, MouseEvent } from "react";
+import React, {
+  useEffect,
+  useState,
+  ChangeEvent,
+  FormEvent,
+  MouseEvent,
+} from "react";
 import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import { Map, TileLayer, Marker } from "react-leaflet";
@@ -6,7 +12,7 @@ import { LeafletMouseEvent } from "leaflet";
 import axios from "axios";
 import api from "../../services/api";
 import logo from "../../assets/logo.png";
-import Dropzone from '../../components/Dropzone';
+import Dropzone from "../../components/Dropzone";
 import "./styles.css";
 
 interface Item {
@@ -40,11 +46,9 @@ interface RootObject {
   q?: any;
 }
 
-
 interface cityResponse {
   nome: string;
 }
-
 
 const CreatePoint = () => {
   const [items, setItems] = useState<Item[]>([]);
@@ -65,7 +69,7 @@ const CreatePoint = () => {
     region: "",
     postcode: "",
     latitude: "",
-    longitude: ""
+    longitude: "",
   });
 
   const [selectedRegion, setSelectedRegion] = useState("0");
@@ -78,7 +82,7 @@ const CreatePoint = () => {
   const [selectedFile, setSelectedFile] = useState<File>();
   const history = useHistory();
 
-   useEffect(() => {
+  useEffect(() => {
     api.get("items").then((response) => {
       setItems(response.data);
     });
@@ -117,37 +121,48 @@ const CreatePoint = () => {
   }, []);
 
   function handleSelectAddress(event: MouseEvent<HTMLOptionElement>) {
-    event.preventDefault();    
+    event.preventDefault();
     setAddress([""]);
-    console.log(event.currentTarget.value);
+    // console.log(event.currentTarget.value);
     axios
-    .get<FinalAddressRes>(
-      `https://api.addy.co.nz/validation?key=c5d5722efb924aad9588470812c3d3ef&address=${event.currentTarget.value}`
-    )
-    .then((response) => {
-        const addressOptions = response.data.address;
-        console.log(addressOptions);
-    });
+      .get<FinalAddressRes>(
+        `https://api.addy.co.nz/validation?key=c5d5722efb924aad9588470812c3d3ef&address=${event.currentTarget.value}`
+      )
+      .then((response) => {
+        const addressOptions = response.data.address.full;
+        const inputValue = document.getElementById(
+          "addressInput"
+        ) as HTMLInputElement;
+        inputValue.value = response.data.address.full;
+        setinitialPosition([
+          Number(response.data.address.y),
+          Number(response.data.address.x),
+        ]);
+        setSelectedPosition([
+          Number(response.data.address.y),
+          Number(response.data.address.x),
+        ]);
+      });
     setFormData({
       ...formData,
       address: event.currentTarget.value,
     });
-    
   }
 
   function handleInputAddress(event: ChangeEvent<HTMLInputElement>) {
     const addressInput = event.target.value;
     if (addressInput.length > 3) {
       axios
-      .get<RootObject>(
-        `https://api.addy.co.nz/search?key=c5d5722efb924aad9588470812c3d3ef&s=${addressInput}`
-      )
-      .then((response) => {
-          const addressOptions = response.data.addresses.map((address) => address.a);
+        .get<RootObject>(
+          `https://api.addy.co.nz/search?key=c5d5722efb924aad9588470812c3d3ef&s=${addressInput}`
+        )
+        .then((response) => {
+          const addressOptions = response.data.addresses.map(
+            (address) => address.a
+          );
           setAddress(addressOptions);
-      });
+        });
     }
-    
   }
 
   function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
@@ -180,28 +195,39 @@ const CreatePoint = () => {
   }
   async function handleSubmit(event: FormEvent) {
     event.preventDefault();
-    const { name, email, address, phone, latitude, longitude, suburb, city, region, postcode  } = formData;
+    const {
+      name,
+      email,
+      address,
+      phone,
+      latitude,
+      longitude,
+      suburb,
+      city,
+      region,
+      postcode,
+    } = formData;
     // const uf = selectedRegion;
     // const city = selectedCity;
     // const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
-    
+
     const data = new FormData();
-      data.append('name', name);
-      data.append('email', email);
-      data.append('phone', phone);
-      data.append('latitude', String(latitude));
-      data.append('longitude', String(longitude));
-      data.append('address', address);
-      data.append('suburb', suburb);
-      data.append('city', city);
-      data.append('region', region);
-      data.append('postcode', postcode);
-      data.append('items', items.join(','));
-      if (selectedFile) {
-        data.append('image', selectedFile);
-      } 
-    
+    data.append("name", name);
+    data.append("email", email);
+    data.append("phone", phone);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("address", address);
+    data.append("suburb", suburb);
+    data.append("city", city);
+    data.append("region", region);
+    data.append("postcode", postcode);
+    data.append("items", items.join(","));
+    if (selectedFile) {
+      data.append("image", selectedFile);
+    }
+
     await api.post("points", data);
     history.push("/success");
   }
@@ -262,23 +288,28 @@ const CreatePoint = () => {
           <div className="field-group">
             <div className="field">
               <label htmlFor="address"></label>
-              <input id="addressInput" type="text" className="field address-main" name="address" placeholder="Type your address..." onChange={handleInputAddress} />
+              <input
+                id="addressInput"
+                type="text"
+                className="field address-main"
+                name="address"
+                placeholder="Type your address..."
+                onChange={handleInputAddress}
+              />
 
               <div>
-                
                 {address.map((address) => (
-                  <option key={address} value={address} className={address.length === 0 ? "" : "address-option"} onClick={handleSelectAddress}>
+                  <option
+                    key={address}
+                    value={address}
+                    className={address.length === 0 ? "" : "address-option"}
+                    onClick={handleSelectAddress}
+                  >
                     {address}
                   </option>
-                ))
-                }
+                ))}
               </div>
-
-
             </div>
-
-
-
           </div>
           <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
             <TileLayer
